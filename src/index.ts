0589a107b071f43537e199d48330b8d32249e741
Luis Coelho <mancoeluis@gmail.com>
2022-09-19 13:55:09 -0300

    ✨ init express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello, ms-banking-lubycash!')
+import express from 'express'
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}.`)
+})

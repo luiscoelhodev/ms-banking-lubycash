76434e9050a21864b9315fb9577c2eda55d6807b
Luis Coelho <mancoeluis@gmail.com>
2022-09-22 22:24:44 -0300

    🚚 rename file

---
+++ src/validators/CustomersControllerValidator.ts
@@ -0,0 +1,20 @@
+import { Status } from '@prisma/client'
+import Joi from 'joi'
+
+const userToCustomerSchema = Joi.object({
+  cpf_number: Joi.string().pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)),
+  average_salary: Joi.number().min(0),
+})
+
+const listAllCustomersSchema = Joi.object({
+  status: Joi.object(Status).optional(),
+  from: Joi.date().optional(),
+  to: Joi.date().optional()
+})
+
+const getCustomerBankStatementSchema = Joi.object({
+  from: Joi.date().optional(),
+  to: Joi.date().optional()
+})
+
+export { userToCustomerSchema, listAllCustomersSchema, getCustomerBankStatementSchema }
--- src/validators/UsersControllerValidator.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi'
-
-const userToCustomerSchema = Joi.object({
-  cpf_number: Joi.string().pattern(new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)),
-  average_salary: Joi.number().min(0),
-})
-
-export { userToCustomerSchema }
